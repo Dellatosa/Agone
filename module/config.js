8690f8d0e0c6d7959e76723779d864788fc7c792
Antony MAILLOT <maillot.antony@orange.fr>
2021-05-31 22:02:42 +0200

    New items sheets

--- module/config.js
+++ module/config.js
@@ -1,6 +1,7 @@
 export const agone = {};
 
 agone.weaponTypes = {
+    none: "",
     pierce: "agone.weapontypes.pierce",
     sharp: "agone.weapontypes.sharp",
     piercesharp: "agone.weapontypes.piercesharp",
@@ -8,7 +9,26 @@ agone.weaponTypes = {
 }
 
 agone.weaponStyles = {
+    none: "",
     melee: "agone.weaponStyles.melee",
     thrown: "agone.weaponStyles.thrown",
     archery: "agone.weaponStyles.archery"
+}
+
+agone.assetFaultTypes = {
+    none: "",
+    chargesociety: "agone.assetFaultTypes.chargesociety",
+    soul: "agone.common.soul",
+    spirit: "agone.common.spirit",
+    body: "agone.common.body",
+    arts: "agone.common.arts",
+    ascendancy: "agone.common.ascendancy",
+    seasons: "agone.common.seasons",
+    flame: "agone.common.flame"
+}
+
+agone.troubleTypes = {
+    none: "",
+    darkness: "agone.common.darkness",
+    perfidy: "agone.common.perfidy",
 }
