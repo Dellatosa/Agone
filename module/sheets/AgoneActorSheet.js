1f01e7e1dea98b3ab0b32e15e406b891d0ff2def
Antony MAILLOT <maillot.antony@orange.fr>
2021-06-08 00:32:25 +0200

    Carac secondaires

--- module/sheets/AgoneActorSheet.js
+++ module/sheets/AgoneActorSheet.js
@@ -20,13 +20,25 @@ export default class AgoneActorSheet extends ActorSheet {
         const actorData = data.data;
         
         // Calcul des scores de bonus d'aspects
-        actorData.flamme.aspectCorps.bonusCorps = actorData.flamme.aspectCorps.corps - actorData.flamme.aspectCorps.corpsNoir;
-        actorData.flamme.aspectEsprit.bonusEsprit = actorData.flamme.aspectEsprit.esprit - actorData.flamme.aspectEsprit.espritNoir;
-        actorData.flamme.aspectAme.bonusAme = actorData.flamme.aspectAme.ame - actorData.flamme.aspectAme.ameNoire;
+        actorData.aspectsFlamme.aspectCorps.bonusCorps = actorData.aspectsFlamme.aspectCorps.corps - actorData.aspectsFlamme.aspectCorps.corpsNoir;
+        actorData.aspectsFlamme.aspectEsprit.bonusEsprit = actorData.aspectsFlamme.aspectEsprit.esprit - actorData.aspectsFlamme.aspectEsprit.espritNoir;
+        actorData.aspectsFlamme.aspectAme.bonusAme = actorData.aspectsFlamme.aspectAme.ame - actorData.aspectsFlamme.aspectAme.ameNoire;
 
         // Calcul des scores de Flamme et Flamme noire
-        actorData.flamme.scoreFlamme = Math.min(actorData.flamme.aspectCorps.corps, actorData.flamme.aspectEsprit.esprit, actorData.flamme.aspectAme.ame);
-        actorData.flamme.scoreFlammeNoire = Math.min(actorData.flamme.aspectCorps.corpsNoir, actorData.flamme.aspectEsprit.espritNoir, actorData.flamme.aspectAme.ameNoire);
+        actorData.aspectsFlamme.flamme = Math.min(actorData.aspectsFlamme.aspectCorps.corps, actorData.aspectsFlamme.aspectEsprit.esprit, actorData.aspectsFlamme.aspectAme.ame);
+        actorData.aspectsFlamme.flammeNoire = Math.min(actorData.aspectsFlamme.aspectCorps.corpsNoir, actorData.aspectsFlamme.aspectEsprit.espritNoir, actorData.aspectsFlamme.aspectAme.ameNoire);
+
+        // Calcul des caractéristiques secondaires
+        actorData.caracSecondaires.seuilBlessureGrave = 0;
+        actorData.caracSecondaires.seuilBlessureCritique = 0;
+        actorData.caracSecondaires.bonusDommages = 0;
+        actorData.caracSecondaires.chargeMax = 0;
+        actorData.caracSecondaires.demiCharge = 0;
+        actorData.caracSecondaires.chargeQuotidienne = 0;
+        actorData.caracSecondaires.emprise = 0;
+        actorData.caracSecondaires.melee = Math.floor((actorData.caracPrimaires.corps.force.valeur + actorData.caracPrimaires.corps.agilite.valeur * 2) / 3);
+        actorData.caracSecondaires.tir = Math.floor((actorData.caracPrimaires.corps.perception.valeur + actorData.caracPrimaires.corps.agilite.valeur) / 2);
+        actorData.caracSecondaires.art = Math.floor((actorData.caracPrimaires.ame.charisme.valeur + actorData.caracPrimaires.ame.creativite.valeur) / 2);
 
         console.log(actorData);
 
