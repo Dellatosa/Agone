baf327de72c6d7da71697fa15816aa2c84263ed0
Antony MAILLOT <maillot.antony@orange.fr>
2022-09-05 21:38:33 +0200

    Macro fix

--- module/agone.js
+++ module/agone.js
@@ -156,7 +156,7 @@ async function createAgoneMacro(data, slot) {
   
     // Create the macro command
     const command = `game.agone.rollItemMacro("${item.name}");`;
-    let macro = game.macros.entities.find(m => (m.name === item.name) && (m.command === command));
+    let macro = game.macros.contents.find(m => (m.name === item.name) && (m.command === command));
     if (!macro) {
       macro = await Macro.create({
         name: item.name,
